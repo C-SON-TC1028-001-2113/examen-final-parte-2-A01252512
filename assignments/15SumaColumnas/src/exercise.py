1c5fc607c8a0ca40dd7d763e99b8479589aa972a
A01252512 <a01252512@itesm.mx>
2021-10-15 18:12:27 +0000

    V1

--- assignments/15SumaColumnas/src/exercise.py
+++ assignments/15SumaColumnas/src/exercise.py
@@ -1,6 +1,22 @@
-
-
 def main():
+    matriz = []
+    lista_suma=[]
+    renglones = int(input())
+    columnas = int(input())
+    for i in range(renglones):
+        lista = []
+        matriz.append(lista)
+        for j in range(columnas):
+            matriz[i].append(int(input()))
+    
+    for i in range(columnas):
+        suma = 0
+        for j in range(renglones):
+            suma = matriz[j][i]+suma
+        lista_suma.append(suma)
+    print(lista_suma)
+  
+
 
 
 
